240e7f2608d75ec698831f85f9d4bbcfdc6478a4
David Liu <liurui81@gmail.com>
2022-11-18 15:39:53 +0800

    splite model info by ModelInfo class

--- onnx/cv/human_segmentation.h
+++ onnx/cv/human_segmentation.h
@@ -20,11 +20,6 @@ namespace onnx
 {
     namespace hs
     {
-        // typedef Eigen::Tensor<float, 3, Eigen::RowMajor> Tensor3f;
-        // typedef Eigen::Tensor<float, 4, Eigen::RowMajor> Tensor4f;
-        // typedef Eigen::array<int, 3> Shape3i;
-        // typedef std::array<int64_t, 4> ModelShape;
-
         class HumanSegmentaion
         {
         public:
@@ -37,17 +32,6 @@ namespace onnx
 
         private:
             const HumanSegModelInfo m_model_info;
-            // static const int WIDTH = 398;
-            // static const int HEIGHT = 224;
-            // static constexpr int SHAPE = WIDTH * HEIGHT;
-            // static constexpr size_t INPUT_TENSOR_SIZE = SHAPE * 3;
-            // static constexpr size_t OUTPUT_TENSOR_SIZE = SHAPE * 2;
-
-            // const ModelShape MODEL_INPUT_SHAPE{1, 3, HEIGHT, WIDTH};
-            // const ModelShape MODEL_OUTPUT_SHAPE{1, 2, HEIGHT, WIDTH};
-
-            // const char *MODEL_INPUT_NAMES = {"x"};
-            // const char *MODEL_OUTPUT_NAMES = {"save_infer_model/scale_0.tmp_1"};
 
             Ort::Env m_ortEnv;
             Ort::AllocatorWithDefaultOptions m_allocator;
