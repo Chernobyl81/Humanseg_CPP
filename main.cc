d3511269b98c5f4b50ab4133d5423fe74acf7b98
David Liu <liurui81@gmail.com>
2022-11-23 17:11:47 +0800

    refact code

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <chrono>
 #include <vector>
 #include <mutex>
@@ -6,9 +7,20 @@
 #include "onnx/cv/human_segmentation.h"
 #include "grpc_client.h"
 
+#define GET_TRANSFORMATION_CODE(x) cv::COLOR_##x
+
+enum ScaleType
+{
+    NONE = 0,
+    VGG = 1,
+    INCEPTION = 2
+};
+
 namespace tc = triton::client;
 using namespace onnx::core;
 using namespace onnx::hs;
+
+typedef std::unique_ptr<tc::InferenceServerGrpcClient> GRPC_CLIENT;
 
 struct ModelInfo
 {
@@ -19,8 +31,14 @@ struct ModelInfo
     std::string input_name_;
     std::string input_format_;
     std::string input_datatype_;
+
+    // channels
     int input_c_;
+
+    // height
     int input_h_;
+
+    // weigh
     int input_w_;
 
     int type1_;
@@ -102,6 +120,8 @@ int matting(char **argv)
 
 bool ParseType(const std::string &dtype, int *type1, int *type3)
 {
+    std::cout << "input dtype " << dtype << std::endl;
+
     if (dtype.compare("UINT8") == 0)
     {
         *type1 = CV_8UC1;
@@ -145,23 +165,20 @@ bool ParseType(const std::string &dtype, int *type1, int *type3)
     return true;
 }
 
-void ParseModelGrpc(
-    const inference::ModelMetadataResponse &model_metadata,
-    const inference::ModelConfigResponse &model_config,
-    const size_t batch_size,
-    ModelInfo *model_info)
+void ParseModelGrpc(const inference::ModelMetadataResponse &model_metadata,
+                    const inference::ModelConfigResponse &model_config,
+                    size_t batch_size,
+                    ModelInfo *model_info)
 {
     if (model_metadata.inputs().size() != 1)
     {
-        std::cerr << "expecting 1 input, got " << model_metadata.inputs().size()
-                  << std::endl;
+        std::cerr << "expecting 1 input, got " << model_metadata.inputs().size() << std::endl;
         exit(1);
     }
 
     if (model_metadata.outputs().size() != 1)
     {
-        std::cerr << "expecting 1 output, got " << model_metadata.outputs().size()
-                  << std::endl;
+        std::cerr << "expecting 1 output, got " << model_metadata.outputs().size() << std::endl;
         exit(1);
     }
 
@@ -173,9 +190,8 @@ void ParseModelGrpc(
     }
 
     auto input_metadata = model_metadata.inputs(0);
-    auto input_config = model_config.config().input(0);
-
     auto output_metadata = model_metadata.outputs(0);
+    auto input_config = model_config.config().input(0);
 
     if (output_metadata.datatype().compare("FP32") != 0)
     {
@@ -184,6 +200,7 @@ void ParseModelGrpc(
                   << output_metadata.datatype() << "'" << std::endl;
         exit(1);
     }
+
     model_info->max_batch_size_ = model_config.config().max_batch_size();
 
     // Model specifying maximum batch size of 0 indicates that batching
@@ -201,7 +218,6 @@ void ParseModelGrpc(
     }
     else
     {
-        //  model_info->max_batch_size_ > 0
         if (batch_size > (size_t)model_info->max_batch_size_)
         {
             std::cerr << "expecting batch size <= " << model_info->max_batch_size_
@@ -209,10 +225,6 @@ void ParseModelGrpc(
             exit(1);
         }
     }
-
-    // Output is expected to be a vector. But allow any number of
-    // dimensions as long as all but 1 is size 1 (e.g. { 10 }, { 1, 10
-    // }, { 10, 1, 1 } are all ok).
 
     // Model input must have 3 dims, either CHW or HWC (not counting the
     // batch dimension), either CHW or HWC
@@ -246,6 +258,8 @@ void ParseModelGrpc(
 
     if (input_config.format() == inference::ModelInput::FORMAT_NHWC)
     {
+        std::cout << "input format is FORMAT_NHWC" << std::endl;
+
         model_info->input_format_ = "FORMAT_NHWC";
         model_info->input_h_ = input_metadata.shape(input_batch_dim ? 1 : 0);
         model_info->input_w_ = input_metadata.shape(input_batch_dim ? 2 : 1);
@@ -253,6 +267,8 @@ void ParseModelGrpc(
     }
     else
     {
+        std::cout << "input format is FORMAT_NCHW" << std::endl;
+
         model_info->input_format_ = "FORMAT_NCHW";
         model_info->input_c_ = input_metadata.shape(input_batch_dim ? 1 : 0);
         model_info->input_h_ = input_metadata.shape(input_batch_dim ? 2 : 1);
@@ -269,32 +285,246 @@ void ParseModelGrpc(
     }
 }
 
-void grpc_test(char **argv)
+void normalize(const cv::Mat &image, Tensor3f &tensor)
 {
-    ModelInfo model_info;
-    tc::Error err;
-    tc::Headers http_headers;
-    std::unique_ptr<tc::InferenceServerGrpcClient> grpc_client;
+    cv::cv2eigen(image, tensor);
+    tensor = tensor / 255.0f;
+    tensor = tensor - 0.5f;
+    tensor = tensor / 0.5f;
+}
 
-    size_t batch_size = 1;
-    bool verbose = true;
-    err = tc::InferenceServerGrpcClient::Create(&grpc_client, "localhost:8001", verbose);
+void Preprocess(const cv::Mat &img, 
+                const std::string &format, 
+                int img_type1, 
+                int img_type3,
+                size_t img_channels, 
+                const cv::Size &img_size, 
+                const ScaleType scale,
+                std::vector<uint8_t> *input_data)
+{
+    // Image channels are in BGR order. Currently model configuration
+    // data doesn't provide any information as to the expected channel
+    // orderings (like RGB, BGR). We are going to assume that RGB is the
+    // most likely ordering and so change the channels to that ordering.
 
+    cv::Mat sample;
+
+    std::cout << "Origin image channels " << img.channels() << std::endl;
+
+    if ((img.channels() == 3) && (img_channels == 1))
+    {
+        cv::cvtColor(img, sample, GET_TRANSFORMATION_CODE(BGR2GRAY));
+    }
+    else if ((img.channels() == 4) && (img_channels == 1))
+    {
+        cv::cvtColor(img, sample, GET_TRANSFORMATION_CODE(BGRA2GRAY));
+    }
+    else if ((img.channels() == 3) && (img_channels == 3))
+    {
+        std::cout << "convert BGR2RGB" << std::endl;
+        cv::cvtColor(img, sample, GET_TRANSFORMATION_CODE(BGR2RGB));
+    }
+    else if ((img.channels() == 4) && (img_channels == 3))
+    {
+        cv::cvtColor(img, sample, GET_TRANSFORMATION_CODE(BGRA2RGB));
+    }
+    else if ((img.channels() == 1) && (img_channels == 3))
+    {
+        cv::cvtColor(img, sample, GET_TRANSFORMATION_CODE(GRAY2RGB));
+    }
+    else
+    {
+        std::cerr << "unexpected number of channels " << img.channels()
+                  << " in input image, model expects " << img_channels << "."
+                  << std::endl;
+        exit(1);
+    }
+
+    cv::Mat sample_resized;
+    if (sample.size() != img_size)
+    {
+        cv::resize(sample, sample_resized, img_size);
+    }
+    else
+    {
+        sample_resized = sample;
+    }
+
+    cv::Mat sample_type;
+    sample_resized.convertTo(
+        sample_type, (img_channels == 3) ? img_type3 : img_type1);
+
+    cv::Mat sample_final;
+    if (scale == ScaleType::INCEPTION)
+    {
+        if (img_channels == 1)
+        {
+            sample_final = sample_type.mul(cv::Scalar(1 / 255.0));
+            sample_final = sample_final - cv::Scalar(0.5);
+            sample_final = sample_final.mul(Scalar(2.0));
+        }
+        else
+        {
+            sample_final = sample_type.mul(cv::Scalar(1 / 255.0));
+            sample_final = sample_final - cv::Scalar(0.5);
+            sample_final = sample_final.mul(Scalar(2.0));
+            // sample_final =
+            //     sample_type.mul(cv::Scalar(1 / 127.5, 1 / 127.5, 1 / 127.5));
+            // sample_final = sample_final - cv::Scalar(1.0, 1.0, 1.0);
+        }
+    }
+    else if (scale == ScaleType::VGG)
+    {
+        if (img_channels == 1)
+        {
+            sample_final = sample_type - cv::Scalar(128);
+        }
+        else
+        {
+            sample_final = sample_type - cv::Scalar(123, 117, 104);
+        }
+    }
+    else
+    {
+        sample_final = sample_type;
+    }
+
+    // Allocate a buffer to hold all image elements.
+    size_t img_byte_size = sample_final.total() * sample_final.elemSize();
+    size_t pos = 0;
+    input_data->resize(img_byte_size);
+
+    // For NHWC format Mat is already in the correct order but need to
+    // handle both cases of data being contigious or not.
+    if (format.compare("FORMAT_NHWC") == 0)
+    {
+        if (sample_final.isContinuous())
+        {
+            memcpy(&((*input_data)[0]), sample_final.datastart, img_byte_size);
+            pos = img_byte_size;
+        }
+        else
+        {
+            size_t row_byte_size = sample_final.cols * sample_final.elemSize();
+            for (int r = 0; r < sample_final.rows; ++r)
+            {
+                memcpy(
+                    &((*input_data)[pos]), sample_final.ptr<uint8_t>(r), row_byte_size);
+                pos += row_byte_size;
+            }
+        }
+    }
+    else
+    {
+        // (format.compare("FORMAT_NCHW") == 0)
+        //
+        // For CHW formats must split out each channel from the matrix and
+        // order them as BBBB...GGGG...RRRR. To do this split the channels
+        // of the image directly into 'input_data'. The BGR channels are
+        // backed by the 'input_data' vector so that ends up with CHW
+        // order of the data.
+        std::vector<cv::Mat> input_bgr_channels;
+        for (size_t i = 0; i < img_channels; ++i)
+        {
+            input_bgr_channels.emplace_back(
+                img_size.height, img_size.width, img_type1, &((*input_data)[pos]));
+            pos += input_bgr_channels.back().total() *
+                   input_bgr_channels.back().elemSize();
+        }
+
+        cv::split(sample_final, input_bgr_channels);
+    }
+
+    if (pos != img_byte_size)
+    {
+        std::cerr << "unexpected total size of channels " << pos << ", expecting "
+                  << img_byte_size << std::endl;
+        exit(1);
+    }
+}
+
+void postprocess(const std::array<float, HumanSegModelInfo::OUTPUT_TENSOR_SIZE> &output,
+                 const cv::Mat &originMat,
+                 const Tensor3f &bgTensor,
+                 cv::Mat &matted)
+{
+    auto start = std::chrono::steady_clock::now();
+    auto size = originMat.size();
+
+    Tensor3f origin_tensor(originMat.cols, originMat.rows, 3);
+    cv::cv2eigen(originMat, origin_tensor);
+
+    std::array<float, HumanSegModelInfo::SHAPE> scroe_map{};
+    auto k = output.data();
+    std::copy(k + HumanSegModelInfo::SHAPE, k + HumanSegModelInfo::OUTPUT_TENSOR_SIZE, scroe_map.data());
+
+    Eigen::TensorMap<Tensor3f> score_tensor{scroe_map.data(), 1, 224, 398};
+
+    // Transpose
+    const Shape3i shuffling({1, 2, 0});
+    Tensor3f scrore_transposed = score_tensor.shuffle(shuffling);
+
+    Mat tmp;
+    cv::eigen2cv(scrore_transposed, tmp);
+    cv::resize(tmp, tmp, size);
+
+    Tensor3f alpha(size.height, size.width, 1);
+    cv::cv2eigen(tmp, alpha);
+
+    Shape3i bcast = {1, 1, 3};
+    Tensor3f ab = alpha.broadcast(bcast);
+
+    Tensor3f comb = ab * origin_tensor + (1 - ab) * bgTensor;
+    Eigen::Tensor<cv::uint8_t, 3, Eigen::RowMajor> results = comb.cast<cv::uint8_t>();
+
+    cv::eigen2cv(results, matted);
+
+    auto end = std::chrono::steady_clock::now();
+    double dr_s = std::chrono::duration<double, std::milli>(end - start).count();
+    std::cout << "Postprocess time: " << dr_s << "ms" << std::endl;
+}
+
+const Tensor3f GenerateBg(cv::Mat &bg, const cv::Size &size)
+{
+    cv::resize(bg, bg, size);
+    static Tensor3f bg_tensor(size.height, size.width, 3);
+    cv::cv2eigen(bg, bg_tensor);
+    return bg_tensor;
+}
+
+GRPC_CLIENT create_grpc_client(
+    const tc::Headers &http_headers,
+    const std::string &url,
+    const size_t batch_size = 1,
+    const bool verbose = false)
+{
+    GRPC_CLIENT client;
+    auto err = tc::InferenceServerGrpcClient::Create(&client, url, verbose);
     if (!err.IsOk())
     {
-        std::cerr << "error: unable to create client for inference: " << err.Message()
+        std::cerr << "Error: unable to create client for inference: "
+                  << err.Message()
                   << std::endl;
         EXIT_FAILURE;
     }
 
-    inference::ModelMetadataResponse model_metadata;
+    return client;
+}
+
+void get_model_info_from_server(GRPC_CLIENT &grpc_client,
+                                ModelInfo &model_info,
+                                const tc::Headers &http_headers,
+                                inference::ModelMetadataResponse &model_metadata,
+                                inference::ModelConfigResponse &model_config,
+                                size_t batch_size = 1)
+{
+    tc::Error err;
     err = grpc_client->ModelMetadata(&model_metadata, "ppseg_onnx", "1", http_headers);
     if (!err.IsOk())
     {
         std::cerr << "error: failed to get model metadata: " << err << std::endl;
     }
 
-    inference::ModelConfigResponse model_config;
     err = grpc_client->ModelConfig(&model_config, "ppseg_onnx", "1", http_headers);
     if (!err.IsOk())
     {
@@ -302,8 +532,10 @@ void grpc_test(char **argv)
     }
 
     ParseModelGrpc(model_metadata, model_config, batch_size, &model_info);
+}
 
-    std::vector<int64_t> shape;
+void model_shape(const ModelInfo &model_info, const size_t batch_size, std::vector<int64_t>& shape)
+{
     // Include the batch dimension if required
     if (model_info.max_batch_size_ != 0)
     {
@@ -321,6 +553,81 @@ void grpc_test(char **argv)
         shape.push_back(model_info.input_h_);
         shape.push_back(model_info.input_w_);
     }
+}
+
+void do_inference(GRPC_CLIENT& grpc_client,
+                  const ModelInfo& model_info,
+                  const std::vector<tc::InferInput *> inputs_ptr, 
+                  const std::vector<const tc::InferRequestedOutput *> outputs_ptr,
+                  const tc::InferOptions& options,
+                  const tc::Headers headers,
+                  bool verbose = false)
+{
+    auto start = std::chrono::steady_clock::now();
+
+    tc::InferResult *result;
+    auto err = grpc_client->Infer(&result, options, inputs_ptr, outputs_ptr, headers);
+    if (!err.IsOk())
+    {
+        std::cerr << "failed sending synchronous infer request: " << err
+                  << std::endl;
+        exit(1);
+    }
+    auto end = std::chrono::steady_clock::now();
+    double dr_s = std::chrono::duration<double, std::milli>(end - start).count();
+    std::cout << "Postprocess time: " << dr_s << "ms" << std::endl;
+
+    if (!result->RequestStatus().IsOk())
+    {
+        std::cerr << "inference  failed with error: " << result->RequestStatus()
+                  << std::endl;
+        exit(1);
+    }
+    std::string datatype;
+    err = result->Datatype(model_info.output_name_, &datatype);
+    if (!err.IsOk())
+    {
+        std::cerr << "unable to get datatype for " << model_info.output_name_ << std::endl;
+        exit(1);
+    }
+
+    if (datatype.compare("FP32") != 0)
+    {
+        std::cerr << "received incorrect datatype for " << model_info.output_name_ << ": "
+                  << datatype << std::endl;
+        exit(1);
+    }
+
+    std::vector<int64_t> shape;
+    err = result->Shape(model_info.output_name_, &shape);
+    if (!err.IsOk())
+    {
+        std::cerr << "unable to get shape for " << model_info.output_name_ << std::endl;
+        exit(1);
+    }
+
+    std::cout << "result shape size " << shape.size() << std::endl;
+    std::cout << "result shape 1 is " << shape.at(0) << "\n"
+              << "result shape 2 is " << shape.at(1) << "\n"
+              << "result shape 3 is " << shape.at(2) << "\n"
+              << "result shape 3 is " << shape.at(3) << std::endl;
+
+
+
+}
+
+void grpc_test(GRPC_CLIENT &grpc_client, 
+                const ModelInfo& model_info, 
+                const tc::Headers& header,
+                const size_t batch_size,
+                const Mat& origin_mat,
+                const Mat& bg,
+                bool verbose = false)
+{
+    tc::Error err;
+    
+    std::vector<int64_t> shape;
+    model_shape(model_info, batch_size, shape);
 
     tc::InferInput *input;
     err = tc::InferInput::Create(&input, model_info.input_name_, shape, model_info.input_datatype_);
@@ -332,7 +639,6 @@ void grpc_test(char **argv)
     std::shared_ptr<tc::InferInput> input_ptr(input);
 
     tc::InferRequestedOutput *output;
-    // Set the number of classification expected
     err = tc::InferRequestedOutput::Create(&output, model_info.output_name_);
     if (!err.IsOk())
     {
@@ -347,19 +653,84 @@ void grpc_test(char **argv)
     tc::InferOptions options("ppseg_onnx");
     options.model_version_ = "1";
 
-    tc::InferResult *result;
-    err = grpc_client->Infer(
-        &result, options, inputs, outputs, http_headers);
-    if (!err.IsOk())
-    {
-        std::cerr << "failed sending synchronous infer request: " << err
-                  << std::endl;
-        exit(1);
-    }
+    // cv::Mat origin_mat = cv::imread(origin_image_name);
+    // auto size = origin_mat.size();
+    // Tensor3f origin_tensor(origin_mat.cols, origin_mat.rows, 3);
+    // cv::cv2eigen(origin_mat, origin_tensor);
+
+    // cv::Mat bg_mat = cv::imread(background_image_name);
+    // auto bgTensor = GenerateBg(bg_mat, size);
+
+    std::vector<uint8_t> input_data;
+    Preprocess(origin_mat, 
+               model_info.input_format_, 
+               model_info.type1_, 
+               model_info.type3_, 
+               model_info.input_c_, 
+               cv::Size(model_info.input_w_, model_info.input_h_), 
+               ScaleType::INCEPTION, 
+               &input_data);
+
+    err = input_ptr->AppendRaw(input_data);
+
+    do_inference(grpc_client, model_info, inputs, outputs, options, header, verbose);
+
+    // size_t kk = 398 * 224 * 2;
+    // const uint8_t *buf;
+    // std::array<float, HumanSegModelInfo::SHAPE> scroe_map{};
+    // result->RawData(model_info.output_name_, &buf, &kk);
+
+    // std::copy(buf + HumanSegModelInfo::SHAPE, buf + HumanSegModelInfo::OUTPUT_TENSOR_SIZE + 1, scroe_map.data());
+    // Eigen::TensorMap<Tensor3f> score_tensor{scroe_map.data(), 1, 224, 398};
+
+    // // Transpose
+    // const Shape3i shuffling({1, 2, 0});
+    // Tensor3f scrore_transposed = score_tensor.shuffle(shuffling);
+
+    // Mat tmp;
+    // cv::eigen2cv(scrore_transposed, tmp);
+    // cv::resize(tmp, tmp, size);
+
+    // Tensor3f alpha(size.height, size.width, 1);
+    // cv::cv2eigen(tmp, alpha);
+
+    // Shape3i bcast = {1, 1, 3};
+    // Tensor3f ab = alpha.broadcast(bcast);
+
+    // Tensor3f comb = ab * origin_tensor + (1 - ab) * bgTensor;
+    // Eigen::Tensor<cv::uint8_t, 3, Eigen::RowMajor> results = comb.cast<cv::uint8_t>();
+
+    // cv::Mat matted;
+    // cv::eigen2cv(results, matted);
+
+    
+
+    // cv::imwrite("/home/david/Desktop/res.jpg", matted);
 }
 
 int main(int argc, char **argv)
 {
-    // matting(argv);
-    grpc_test(argv);
+    size_t batch_size = std::stoi(argv[5]);
+
+    bool verbose = false;
+    if (std::string(argv[2]).compare("verbose") == 0)
+        verbose = true;
+    else
+        verbose = false;
+
+    tc::Headers headers;
+    auto client = create_grpc_client(headers, argv[1], batch_size, verbose);
+
+    inference::ModelMetadataResponse model_metadata;
+    inference::ModelConfigResponse model_config;
+    ModelInfo model_info;
+    get_model_info_from_server(client, model_info, headers, model_metadata, model_config, batch_size);
+
+    std::vector<int64_t> shape;
+    model_shape(model_info, batch_size, shape);
+
+    Mat origin_image = cv::imread(argv[3]);
+    Mat bg = cv::imread(argv[4]);
+
+    grpc_test(client, model_info, headers, batch_size, origin_image, bg, verbose);
 }
--- run.sh
+++ run.sh
@@ -4,9 +4,17 @@ rm -rf build
 cmake -B build
 cmake --build build
 
+# build/PP_HumanSeg_CPP \
+# /home/david/Projects/Humanseg_CPP/model_repository/ppseg_onnx/1/model.onnx \
+# /home/david/Desktop/R-C.jpg \
+# images/background.jpg \
+# 2 \
+# CUDA
+
 build/PP_HumanSeg_CPP \
-/home/david/Projects/Humanseg_CPP/model_repository/ppseg_onnx/1/model.onnx \
+localhost:8001 \
+not_verbose \
 /home/david/Desktop/R-C.jpg \
 images/background.jpg \
-2 \
-CUDA
+1
+
